5d080498c1bf64e8b892da709f545dc6732c12ce
tonoutt <antointt@gmail.com>
2018-10-12 07:59:43 +0200

    fin

--- depute1.1.rb
+++ depute1.1.rb
@@ -2,22 +2,36 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-def députés_list()
+def deputes_list
 	page = Nokogiri::HTML(open("https://www.voxpublic.org/spip.php?page=annuaire&cat=deputes&pagnum=600"))
 	@deputes_name = []
-	page.xpath('/html/body/div[1]/main/div/div/div/ul[1]/li[1]/h2').each do |x|
-	@deputes_name << x.text
+	page.xpath('//h2').each do |x|
+		if x.to_s.include?('class="titre_normal"')
+			@deputes_name << x.text.gsub('Mme ', '').gsub('M ', '')
+		end
 	end
 	return @deputes_name
 end
+
+
 def deputes_email
 	page = Nokogiri::HTML(open("https://www.voxpublic.org/spip.php?page=annuaire&cat=deputes&pagnum=600"))
 	@deputes_emails = []
-	page.xpath('/html/body/div[1]/main/div/div/div/ul[1]/li[5]/a[1]').each do |x|
-	@deputes_emails << x.text
+	page.xpath('//a').each do |x|
+		if x.to_s.include?("@assemblee-nationale.fr") && x.text.to_s.include?('secretariat-blanchet') == false
+			@deputes_emails << x.text
+		end
 	end
 	return @deputes_emails
 end
 
-hashi = Hash[députés_list.zip(deputes_email)]
-puts hashi
+nom = deputes_list
+email = deputes_email
+i = 0
+tab = []
+deputes_email.each do
+	tab << { "name" => nom[i], "email" => email[i] }
+	i += 1
+end
+
+puts tab
